a76291276ca0cf089fb44a11d0f720039d56a01b
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2024-11-27 22:01:37 -0300

    feat: add folders (components/guards/interfaces/pages/services)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-idwm',
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pruebacorta2';
+
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
+
